97c22da72c90c1cdcc82c447fc5021ab5e5a98f2
sheryn070204 <sheryn.cuabo@urios.edu.ph>
2025-05-13 06:24:44 +0000

    frontend

---
+++ script/app.js
@@ -0,0 +1,8 @@
+Alpine.store("settings", {
+    apiBaseUrl: "http://172.17.100.14:3342/cuabo2/api",
+    appName: "exam application",
+    chatApiBaseUrl: "http://172.17.100.14:3342/cuabo2/api/exam",  // merged chat info
+    chatAppName: "chat application",  // merged chat app name
+    shopApiBaseUrl: "http://172.17.100.14:3342/cuabo2/api/shop",  // merged shop info
+    shopAppName: "shop application",  // merged shop app name
+});
